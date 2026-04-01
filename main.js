5fddf03fd3125bc06666cd3e8ff93b4e48728e0c
Vincent <vin.rothweiler.25@lehre.mosbach.dbhw.de>
2026-04-01 18:27:23 +0200

    JS TImer funktioniert, versuche noch Einbindung zum Ausführen von Funktionen
    Zeigt aktuell im HTML einen Timer an (Testweise zählt dieser von 10 runter)

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 console.log("Hello, World!");
+console.log("Hello, World!");
+
+document.getElementById("btn1").addEventListener("click", testeTimer);
+
+
+
+function timer(t, textID, funcName) {
+    console.log("in Timer");
+
+    let timerVar = setInterval(function() {
+            document.getElementById(textID).innerHTML = t;
+            if (t == 0) {
+                clearInterval(timerVar)
+                
+            }
+            t = t-1; 
+            
+        }, 1000);
+    
+}
+
+
+function testeTimer() {
+    console.log("Teste Timer")
+    timer(10, "Testtext")
+
+    
+}
